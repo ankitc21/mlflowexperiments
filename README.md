6daf76a31699b094b202ddd1e6af13b7058aae24
ankitc21 <ankit004c@gmail.com>
2025-07-17 21:07:16 +0530

    The model is created'

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### MLFLOW experiments
+
+import dagshub
+dagshub.init(repo_owner='ankitc21', repo_name='mlflowexperiments', mlflow=True)
+
--- app.py
+++ app.py
@@ -13,6 +13,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 from urllib.parse import urlparse
 import mlflow
+import dagshub
+dagshub.init(repo_owner='ankitc21', repo_name='mlflowexperiments', mlflow=True)
+
 from mlflow.models import infer_signature
 import mlflow.sklearn
 
@@ -80,8 +83,8 @@ if __name__ == "__main__":
 
         ## For Remote server only(DAGShub)
 
-        #remote_server_uri="https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
-        #mlflow.set_tracking_uri(remote_server_uri)
+        remote_server_uri="https://dagshub.com/ankitc21/mlflowexperiments.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
